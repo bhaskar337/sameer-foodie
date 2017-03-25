0a1e5e0cae5d9710b0b3d59b9aa2e07e0bc54a51
Aashish Nehete <ashnehete@gmail.com>
2017-03-25 19:12:28 +0530

    routes

--- routes/index.js
+++ routes/index.js
@@ -6,28 +6,4 @@ router.get('/', function (req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
-router.get('/profile', function (req, res, next) {
-
-    var id = 1;
-    getProfile(id, function (user) {
-        res.render('profile', user);
-    });
-});
-
 module.exports = router;
-
-
-
-function getProfile(id, callback) {
-    // get all profile info
-    var result =
-        {
-            _id: 1,
-            name: "Bhaskar Barua",
-            items: [
-                { item_id: 2, name: "Eggs", quantity: 5 },
-                { item_id: 12, name: "Bread", quantity: 2 }
-            ]
-        };
-    return callback(result)
-}
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,30 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
+});
+
+router.get('/profile', function (req, res, next) {
+
+    var id = 1;
+    getProfile(id, function (user) {
+        res.render('profile', user);
+    });
 });
 
 module.exports = router;
+
+function getProfile(id, callback) {
+    // get all profile info
+    var result =
+        {
+            _id: 1,
+            name: "Bhaskar Barua",
+            items: [
+                { item_id: 2, name: "Eggs", quantity: 5 },
+                { item_id: 12, name: "Bread", quantity: 2 }
+            ]
+        };
+    return callback(result)
+}
