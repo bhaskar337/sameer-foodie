0644674eef4679697134adcf2a515422c981cd6f
Aashish Nehete <ashnehete@gmail.com>
2017-03-26 13:33:18 +0530

    Final

--- routes/users.js
+++ routes/users.js
@@ -25,46 +25,46 @@ router.get('/organise', function (req, res, next) {
             return b.ratio-a.ratio;
         });
 
-        results=[
-            {
-                recipe: {
-                    _id:"58d7241d83868606bc3501da",
-                    name:"Gajar Halwa",
-                    ingredients:[
-                    {
-                        item_id:"58d65238bed2c11954911f3e",
-                        name:"Carrot",
-                        quantity:1
-                    },
-                    {
-                        item_id:"58d65256bed2c11954911f3f",
-                        name:"Sugar",
-                        quantity:3
-                    }]
-                },
-                commonValues: ["58d65238bed2c11954911f3e"],
-                ratio: 0.7
-            },
-            {
-                recipe: {
-                    _id:"58d7241d83868606bc3501da",
-                    name:"Chocolate Brownie",
-                    ingredients:[
-                    {
-                        item_id:"58d65238bed2c11954911f3e",
-                        name:"Cocoa Powder",
-                        quantity:1
-                    },
-                    {
-                        item_id:"58d65256bed2c11954911f3f",
-                        name:"Milk Powder",
-                        quantity:3
-                    }]
-                },
-                commanValues: ["58d65238bed2c11954911f3e"],
-                ratio: 0.3
-            }
-        ];
+        // results=[
+        //     {
+        //         recipe: {
+        //             _id:"58d7241d83868606bc3501da",
+        //             name:"Gajar Halwa",
+        //             ingredients:[
+        //             {
+        //                 item_id:"58d65238bed2c11954911f3e",
+        //                 name:"Carrot",
+        //                 quantity:1
+        //             },
+        //             {
+        //                 item_id:"58d65256bed2c11954911f3f",
+        //                 name:"Sugar",
+        //                 quantity:3
+        //             }]
+        //         },
+        //         commonValues: ["58d65238bed2c11954911f3e"],
+        //         ratio: 0.7
+        //     },
+        //     {
+        //         recipe: {
+        //             _id:"58d7241d83868606bc3501da",
+        //             name:"Chocolate Brownie",
+        //             ingredients:[
+        //             {
+        //                 item_id:"58d65238bed2c11954911f3e",
+        //                 name:"Cocoa Powder",
+        //                 quantity:1
+        //             },
+        //             {
+        //                 item_id:"58d65256bed2c11954911f3f",
+        //                 name:"Milk Powder",
+        //                 quantity:3
+        //             }]
+        //         },
+        //         commanValues: ["58d65238bed2c11954911f3e"],
+        //         ratio: 0.3
+        //     }
+        // ];
         res.render('omm',{results:results});
     });
 });
